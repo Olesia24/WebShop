95989818bf32e9a5ec99ce92b4fe3174fdaa63f6
olesiavoprykova <olesia.voprikova@yahoo.com>
2024-04-02 16:38:15 +0200

    first commit

--- src/test/java/hw_03/UniqueElements_cssSelector.java
+++ src/test/java/hw_03/UniqueElements_cssSelector.java
@@ -7,7 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class HW_03_uniqueElements {
+public class UniqueElements_cssSelector {
     WebDriver driver;
     @BeforeMethod
     public void setUp(){
@@ -100,3 +100,4 @@ public class HW_03_uniqueElements {
     public void tearDown(){ driver.quit();}
 
 }
+
